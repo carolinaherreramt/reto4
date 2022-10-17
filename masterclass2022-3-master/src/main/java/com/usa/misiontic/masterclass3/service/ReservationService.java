00af2923d142a17b548ded290ae95a24f0e7e1f2
carolinaherreramt <113864693+carolinaherreramt@users.noreply.github.com>
2022-10-16 22:34:28 -0500

    Add files via upload

---
+++ masterclass2022-3-master/src/main/java/com/usa/misiontic/masterclass3/service/ReservationService.java
@@ -0,0 +1,64 @@
+package com.usa.misiontic.masterclass3.service;
+
+
+import com.usa.misiontic.masterclass3.entities.Admin;
+import com.usa.misiontic.masterclass3.entities.Reservation;
+import com.usa.misiontic.masterclass3.repository.AdminRepository;
+import com.usa.misiontic.masterclass3.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReservationService {
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    public List<Reservation> getAll() {
+        return reservationRepository.getAll();
+    }
+
+    public Optional<Reservation> getReservation(int id) {
+        return reservationRepository.getReservation(id);
+    }
+    public Reservation save(Reservation reservation){
+        if(reservation.getIdReservation()==null) {
+            return reservationRepository.save(reservation);
+        }else{
+            Optional<Reservation>reservationEncontrado=getReservation(reservation.getIdReservation());
+            if(reservationEncontrado.isEmpty()){
+                return reservationRepository.save(reservation);
+            }else{
+                return reservation;
+            }
+        }
+    }
+    public Reservation update(Reservation reservation){
+        if (reservation.getIdReservation()!=null){
+            Optional<Reservation> reservationEncontrado=getReservation(reservation.getIdReservation());
+            if(!reservationEncontrado.isEmpty()){
+                if(reservation.getStartDate()!=null){
+                    reservationEncontrado.get().setStartDate(reservation.getStartDate());
+                }
+                if (reservation.getDevolutionDate()!=null){
+                    reservationEncontrado.get().setDevolutionDate(reservation.getDevolutionDate());
+                }
+                if (reservation.getStatus()!=null){
+                    reservationEncontrado.get().setStatus(reservation.getStatus());
+                }
+                return reservationRepository.save(reservationEncontrado.get());
+
+            }
+        }
+        return reservation;
+    }
+    public boolean deleteReservation(int id){
+        Boolean respuesta= getReservation(id).map(reservation-> {
+            reservationRepository.delete(reservation);
+            return true;
+        }).orElse(false);
+        return respuesta;
+    }
+}
